b1f3a91a799f156166fdc75eac3d57da1547634c
Chasmical <hillankeyxl2@yandex.ru>
2024-09-13 21:38:50 +0700

    Add support for more older target frameworks

---
+++ src/InlineIL/_Shims.cs
@@ -0,0 +1,10 @@
+﻿#if NETSTANDARD1_0
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.InteropServices;
+
+#pragma warning disable CA1008
+#pragma warning disable CS1591
+
+public enum UnmanagedType;
+public enum CallingConvention;
+#endif
